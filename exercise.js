4032809dd0e3272c2f0bfe3110a1bced173b1fc6
Matt <Matt.1022@icloud.com>
2021-06-14 13:42:19 -0500

    added a function to js file

--- exercise.js
+++ exercise.js
@@ -2,3 +2,6 @@
 //Create a function that receives two numbers and returns the substraction
 //Create a function that receives two numbers and returns the multiplication
 //Create a function that receives two numbers and returns the division
+function addtion(n1,n2){
+    return n1 + n2;
+}
