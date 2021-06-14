2ca751475812cb4e4d61fb29c19641255c2293f9
Josue <josue.rodriguez1551@gmail.com>
2021-06-14 12:43:46 -0600

    Created a function to substract two numbers

--- exercise.js
+++ exercise.js
@@ -1,7 +1,11 @@
 //Create a function that receives two numbers and returns the sum
+function addtion(n1, n2) {
+  return n1 + n2;
+}
+
 //Create a function that receives two numbers and returns the substraction
+function substraction(n1, n2) {
+  return n1 - n2;
+}
 //Create a function that receives two numbers and returns the multiplication
 //Create a function that receives two numbers and returns the division
-function addtion(n1,n2){
-    return n1 + n2;
-}
