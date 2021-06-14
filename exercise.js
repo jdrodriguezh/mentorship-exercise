6e37f5c047d1e6be1065ffde2154351b5f100947
Josue <josue.rodriguez1551@gmail.com>
2021-06-14 12:33:10 -0600

    Initial commit, uploading exercise

---
+++ exercise.js
@@ -0,0 +1,4 @@
+//Create a function that receives two numbers and returns the sum
+//Create a function that receives two numbers and returns the substraction
+//Create a function that receives two numbers and returns the multiplication
+//Create a function that receives two numbers and returns the division
